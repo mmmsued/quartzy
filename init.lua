cb3f4e50e127252e57cfcbc8eaa9ece91e543d90
mmmsued <75369513+mmmsued@users.noreply.github.com>
2025-09-11 16:50:53 +0200

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- Author: This mod was made by Norbert Thien, multimediamobil – Region Süd (mmmsued), 2025
+-- Code: Except otherwise specified, all code in this project is licensed as LGPLv3.
+-- Media: Except otherwise specified, all media and any other content in this project which is not source code is licensed as CC BY SA 3.0. 
+
+dofile(minetest.get_modpath("quartzy").."/quartzy_chiseled.lua")
+dofile(minetest.get_modpath("quartzy").."/quartzy_pillar.lua")
+dofile(minetest.get_modpath("quartzy").."/quartzy_tile.lua")
---
+++ quartzy_chiseled.lua
@@ -0,0 +1,56 @@
+-- Author: This mod was made by Norbert Thien, multimediamobil – Region Süd (mmmsued), 2025
+-- Code: Except otherwise specified, all code in this project is licensed as LGPLv3.
+-- Media: Except otherwise specified, all media and any other content in this project which is not source code is licensed as CC BY SA 3.0. 
+
+local setting_for_inventory = tonumber(minetest.settings:get("quartzy_disable_for_inventory") or 1)
+
+local definition = {    -- Beginn verschachtelte Tabelle
+	{"quartzy_chiseled_white", "Chiseled white"}, -- 1. Eintrag der Unter-Tabelle für Node-Name, Textur, Rezept - 2. Eintrag für Description
+    {"quartzy_chiseled_light_gray", "Chiseled light gray"},
+	{"quartzy_chiseled_gray", "Chiseled gray"},
+	{"quartzy_chiseled_dark_gray", "Chiseled dark gray"},
+	{"quartzy_chiseled_pink", "Chiseled pink"},
+	{"quartzy_chiseled_dark_pink", "Chiseled dark pink"}
+} -- Ende verschachtelte Tabelle
+
+for _, row in ipairs(definition) do -- Schleife zum Auslesen der Tabelle »definition«, _ (Unterstrich): übliches formales Vorgehen, wenn ein Rückgabewert nicht benötigt wird, aber abgefangen werden muss
+
+	local name = row[1]			-- ersten Eintrag aus der inneren Tabelle auslesen und der lokalen Variable »name« zuweisen (der Variablenname könnte auch anders lauten)
+	local description = row[2]	-- zweiten Eintrag aus der inneren Tabelle auslesen und der lokalen Variable »description« zuweisen (der Variablenname könnte auch anders lauten)
+
+	minetest.register_node("quartzy:" .. name, {		-- zusammengesetzten Node-Namen generieren
+		description = "Quartzy " .. description,	-- zusammengesetzte Beschreibung generieren
+		drawtype = "nodebox",
+		paramtype2 = "facedir",
+		tiles = {
+			name .. "_top.png", name .. "_top.png", name .. "_side.png"	-- zusammengesetzten Textur-Namen generieren
+		},
+		groups = {
+			cracky = 3,
+            stone = 1
+		},
+		sounds = default.node_sound_stone_defaults()
+	})
+
+
+    -- Zugriff auf den Mod »moreblocks« (genauer auf »stairsplus« - siehe dort »API.md«) - in der »mod.conf« muss »optional_depends = moreblocks« stehen
+	if minetest.get_modpath("moreblocks") then
+	    stairsplus:register_all("quartzy", name, "quartzy:" .. name, {	-- zusammengesetzte Namen generieren
+            description = "Quartzy " .. description,	-- zusammengesetzte Beschreibung generieren
+			drawtype = "nodebox",
+			paramtype2 = "facedir",
+			tiles = {
+				name .. "_top.png", name .. "_top.png", name .. "_side.png"	-- zusammengesetzten Textur-Namen generieren
+			},
+			paramtype = "light",	-- verhindert u. a. fehlerhafte Lichtberechnung bei Quadern ohne Kantenlänge 1x1x1. Nachteil: Block wird insgesamt lichtdurchlässing
+			paramtype2 = "facedir",	-- macht Objekt mit Schraubendreher rotierbar und richtet -Z-Seite beim Ablegen immer in Richtung des Ablegenden
+		    groups = {
+			    cracky = 3,
+                stone = 1,
+                not_in_creative_inventory = setting_for_inventory   -- verhindert, dass alle Formen, die die Kreissäge produzieren kann, im Kreativ-Modus das Inventar überfluten
+		    },
+			sounds = default.node_sound_stone_defaults()
+	    })
+	end -- Ende der if-Abfrage zu minetest.get_modpath() - Start in Zeile 35
+
+end     -- Ende for-Schleife (Start in Zeile 16)
