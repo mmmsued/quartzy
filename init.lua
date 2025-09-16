0d79e8c9c2b3733ee3331fa0840fd7bb013939ec
mmmsued <75369513+mmmsued@users.noreply.github.com>
2025-09-16 12:06:18 +0200

    translator inserted

--- init.lua
+++ init.lua
@@ -2,14 +2,16 @@
 -- Code: Except otherwise specified, all code in this project is licensed as LGPLv3.
 -- Media: Except otherwise specified, all media and any other content in this project which is not source code is licensed as CC BY SA 3.0. 
 
+local S = minetest.get_translator("quartzy")
 
 --  Quartzy Crafting Material
 minetest.register_craftitem("quartzy:quartzy_raw", {
-	description = "Quartzy Raw",
+	description = S("Quartzy Raw"),
 	inventory_image = "quartzy_raw.png",
 })
 
 
+dofile(minetest.get_modpath("quartzy").."/quartzy_brick.lua")
 dofile(minetest.get_modpath("quartzy").."/quartzy_chiseled.lua")
 dofile(minetest.get_modpath("quartzy").."/quartzy_pillar.lua")
 dofile(minetest.get_modpath("quartzy").."/quartzy_plain.lua")
